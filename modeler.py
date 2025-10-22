9a87a41406a1c0dd899384e7643e5279c030dd62
Alex Caramanico <160052029+AlexCaraman@users.noreply.github.com>
2025-10-22 16:13:15 -0400

    Pipeline files - still work in progress

---
+++ modeler.py
@@ -0,0 +1,6 @@
+def model_fitter(df,forest=None,logistic=None,gradient=None,):
+    # df for the data to which the model is to be fit
+    # forest: list of (hyper)parameters to fit with
+    # logistic: list of (hyper)parameters to fit with
+    # gradient: list of (hyper)parameters to fit with
+    pass
